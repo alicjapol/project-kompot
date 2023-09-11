70ee2e1b651323ef865f7531582b7d964a411314
alicjapol <alicjaxpoltorak@gmail.com>
2023-09-11 13:39:20 +0200

    sekcja comments i contact
    do comments dodalam stylowanie, ni chuj nie wiem co zrobic zeby js w sliderze dzialal. w adresie nic nie zmienilam, nie wiem za bardzo co bym mogła. dodalam skrypt js z tutorialu z yt xD

---
+++ src/js/swiper.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
